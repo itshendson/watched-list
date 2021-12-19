1b952d483736344f5b5518551aee489056aaf5d8
Hendson <hendsonszeto@live.com>
2021-12-19 12:25:39 -0800

    Fixed mispelling in variable names

--- server.js
+++ server.js
@@ -2,14 +2,16 @@ const express = require('express');
 const PORT = process.env.PORT || 3000;
 const app = express();
 
-const animeRoutes = require('./routes/anime');
-const bookRoutes = require('./routes/books');
-const movieRoutes = require('./routes/television');
-const gameRoutes = require('./routes/games');
+const animeRoute = require('./routes/anime');
+const bookRoute = require('./routes/books');
+const televisionRoute = require('./routes/television');
+const gameRoute = require('./routes/games');
 
 app.get('/', (req, res) => {
-    res.send('Hello World')
+    res.send('Hello Worlds!')
 });
+
+app.use('/anime', animeRoute);
 
 app.listen(PORT, (err)=> {
     if (err) console.log(err);
