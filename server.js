27011d3a28409f182b96076e313f2dbc264ec2ad
Hendson <hendsonszeto@live.com>
2021-12-19 12:07:40 -0800

    Creating routes for each media type

--- server.js
+++ server.js
@@ -1,12 +1,17 @@
 const express = require('express');
-
 const PORT = process.env.PORT || 3000;
 const app = express();
 
+const animeRoutes = require('./routes/anime');
+const bookRoutes = require('./routes/books');
+const movieRoutes = require('./routes/television');
+const gameRoutes = require('./routes/games');
+
 app.get('/', (req, res) => {
-    res.send('Hello World');
+    res.send('Hello World')
 });
 
-app.listen(PORT, ()=> {
+app.listen(PORT, (err)=> {
+    if (err) console.log(err);
     console.log(`Listening on port ${PORT}`)
 });
