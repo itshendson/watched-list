0e2ab90f6012d724ac8e6fab16dc36153315ed02
Hendson <hendsonszeto@live.com>
2021-12-22 22:02:59 -0800

    Successfully tested getting anime from Jikan API

--- routes/anime.js
+++ routes/anime.js
@@ -1,6 +1,15 @@
-import express from 'express';
+import express, { response } from 'express';
+import fetch from 'node-fetch';
 export const router = express.Router();
 
-router.get('/', (req, res) => {
-    res.send('Anime!!!')
-})
+router.get('/', async (req, res) => {
+    const ani_url = 'https://api.jikan.moe/v3/search/anime?q=death_note&limit=2';
+    const options = {
+        method: 'GET'
+    };
+    const ani_res = await fetch(ani_url, options); // Fetches API
+    const ani_res_json = await ani_res.json(); // Converts API to JSON file
+    const ani_res_obj = ani_res_json.results[0].title; // Gets specific property from JSON file
+    console.log(ani_res_obj);
+    res.send(ani_res_obj);
+});
