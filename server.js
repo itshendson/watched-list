72610a576868243a467a55fa463abfacf1080bc8
Hendson <hendsonszeto@live.com>
2021-12-27 13:40:19 -0800

    Connect to mongoose database

--- server.js
+++ server.js
@@ -1,11 +1,17 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
 const PORT = process.env.PORT || 3000;
-const app = express();
+const DB_STRING = process.env.DB_STRING;
 
 import { router as animeRoute } from './routes/anime.js';
 import { router as bookRoute } from './routes/books.js';
 import { router as televisionRoute } from './routes/television.js';
 import { router as gameRoute } from './routes/games.js';
+
+const app = express();
+
+dotenv.config();
 
 app.get('/', (req, res) => {
     res.send('Hello Worlds!')
@@ -15,6 +21,12 @@ app.use('/anime', animeRoute);
 app.use('/books', bookRoute);
 app.use('/television', televisionRoute);
 app.use('/game', gameRoute);
+
+mongoose.connect(
+    process.env.DB_STRING,
+    { useNewUrlParser: true},
+    console.log('DB connected!')
+);
 
 app.listen(PORT, (err)=> {
     if (err) console.log(err);
