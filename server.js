0ac84e26c8a686ddc909a30c3ff057311de59835
Hendson <hendsonszeto@live.com>
2021-12-28 11:19:25 -0800

    Refactored module to be more organized

--- server.js
+++ server.js
@@ -1,16 +1,14 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
-const PORT = process.env.PORT || 3000;
-const DB_STRING = process.env.DB_STRING;
 
 import { router as animeRoute } from './routes/anime.js';
 import { router as bookRoute } from './routes/books.js';
 import { router as televisionRoute } from './routes/television.js';
 import { router as gameRoute } from './routes/games.js';
 
+const PORT = process.env.PORT || 3000;
 const app = express();
-
 dotenv.config();
 
 app.get('/', (req, res) => {
@@ -22,13 +20,7 @@ app.use('/books', bookRoute);
 app.use('/television', televisionRoute);
 app.use('/game', gameRoute);
 
-mongoose.connect(
-    process.env.DB_STRING,
-    { 
-        useNewUrlParser: true,
-        useUnifiedTopology: true
-    }
-);
+mongoose.connect(process.env.DB_STRING, {useNewUrlParser: true, useUnifiedTopology: true});
 
 const db = mongoose.connection;
 db.on("error", console.error.bind(console, "connection error: "));
