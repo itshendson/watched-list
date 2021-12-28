1127a868beb41649a041ee59f1fbd90a84767093
Hendson <hendsonszeto@live.com>
2021-12-28 11:18:42 -0800

    Experimented with Jikan API to get more results

--- routes/anime.js
+++ routes/anime.js
@@ -3,13 +3,16 @@ import fetch from 'node-fetch';
 export const router = express.Router();
 
 router.get('/', async (req, res) => {
-    const ani_url = 'https://api.jikan.moe/v3/search/anime?q=death_note&limit=2';
+    const ani_url = 'https://api.jikan.moe/v3/search/anime?q=death_note&limit=8';
     const options = {
         method: 'GET'
     };
     const ani_res = await fetch(ani_url, options); // Fetches API
     const ani_res_json = await ani_res.json(); // Converts API to JSON file
-    const ani_res_obj = ani_res_json.results[0].title; // Gets specific property from JSON file
-    console.log(ani_res_obj);
+    const ani_res_obj = ani_res_json.results; // Gets specific property from JSON file
     res.send(ani_res_obj);
 });
+
+router.post('/', (req, res) => {
+
+})
