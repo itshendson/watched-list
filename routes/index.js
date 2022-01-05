eade8bcb9f634f6ec867f420a0457723a0e06b29
Hendson <hendsonszeto@live.com>
2022-01-05 11:25:36 -0800

    added more routes to index.js

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/', (req, res) => {
-    res.send('Hello index.js!')
+router.get('/login', (req, res) => {
+    res.send('Please login to access your Watched List')
+})
+
+router.get('/mylist', (req, res) => {
+    res.send('Create, edit, remove your Watched List')
 })
 
 module.exports = router;
