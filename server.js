f666a2063972b1492cef78adc182bc2487164c58
Hendson <hendsonszeto@live.com>
2022-01-05 11:24:30 -0800

    fixed typo for a comment

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// Session
+// Sessions
 app.use(session({
   secret: 'keyboard cat',
   resave: false,
