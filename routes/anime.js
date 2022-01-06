e3afb78aa58c1bae42c264cd7806ce573da1337a
Hendson <hendsonszeto@live.com>
2022-01-06 00:02:42 -0800

    enabled res.send in anime route for testing

--- routes/anime.js
+++ routes/anime.js
@@ -4,16 +4,16 @@ const fetch = require('node-fetch');
 
 const AnimeModel = require('../models/Anime');
 
-// router.get('/', async (req, res) => {
-//     const ani_url = 'https://api.jikan.moe/v3/search/anime?q=death_note&limit=8';
-//     const options = {
-//         method: 'GET'
-//     };
-//     const ani_res = await fetch(ani_url, options); // Fetches API
-//     const ani_res_json = await ani_res.json(); // Converts API to JSON file
-//     const ani_res_obj = ani_res_json.results; // Gets specific property from JSON file
-//     res.status(200).send(ani_res_obj);
-// });
+router.get('/', async (req, res) => {
+    const ani_url = 'https://api.jikan.moe/v3/search/anime?q=death_note&limit=8';
+    const options = {
+        method: 'GET'
+    };
+    const ani_res = await fetch(ani_url, options); // Fetches API
+    const ani_res_json = await ani_res.json(); // Converts API to JSON file
+    const ani_res_obj = ani_res_json.results; // Gets specific property from JSON file
+    res.status(200).send(ani_res_obj);
+});
 
 router.post('/', async (req, res) => {
     const animeModel = new AnimeModel({
