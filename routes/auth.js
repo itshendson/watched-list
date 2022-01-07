2617b45762eaad1d442d9eedf552b600c0dffb6b
Hendson <hendsonszeto@live.com>
2022-01-07 00:42:46 -0800

    implemented logout functionality

--- routes/auth.js
+++ routes/auth.js
@@ -2,13 +2,17 @@ const express = require('express');
 const passport = require('passport');
 const router = express.Router();
 
-
 router.get('/google', passport.authenticate('google', { scope: ['email', 'profile'] }));
 
 router.get('/google/callback',
   passport.authenticate('google', { failureRedirect: '/login', failureMessage: true }),
   function(req, res) {
     res.redirect('/mylist');
+});
+
+router.get('/logout', (req, res) => {
+  req.logout();
+  res.redirect('/');
 });
 
 module.exports = router;
