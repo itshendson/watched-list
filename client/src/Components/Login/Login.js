b06aa0c194c32afd86ea9e9c9bc6cdb9dad6dd43
Hendson <hendsonszeto@live.com>
2022-01-15 13:51:23 -0800

    Create empty or basic Component for frontend

--- client/src/Components/Login/Login.js
+++ client/src/Components/Login/Login.js
@@ -8,7 +8,7 @@ function Login() {
         <p>Watched List is an app to store your favorite television show, books, anime, and video games. Get started by logging in through Google.</p>
       </div>
       <div className="section">
-        <a href="/auth/google" class="btn red darken-1">
+        <a href="http://localhost:3080/auth/google" class="btn red darken-1">
           <i class="fab fa-google left"></i> Log In With Google
         </a>
       </div>
---
+++ client/src/Components/Search/Search.js
@@ -0,0 +1,11 @@
+function Search() {
+  return (
+    <div className="Search-container">
+      <div className="section">
+        <input id="search-bar"></input>
+      </div>
+    </div>
+  );
+}
+
+export default Search;
