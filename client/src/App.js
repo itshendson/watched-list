13691b3d16d9ca5242bc963f66bb09f940e72169
Hendson <hendsonszeto@live.com>
2022-01-15 13:50:54 -0800

    Remove react boilerplate logo

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import Login from './Components/Login/Login';
 import './App.css';
 
@@ -6,9 +5,6 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-      </header>
       <Login />
     </div>
   );
