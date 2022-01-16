084902fc01cecba9f98a57f9dfc50ae69bac027b
Hendson <hendsonszeto@live.com>
2022-01-16 15:09:09 -0800

    Add elements and styling to Header Component

--- client/src/Components/Header/Header.js
+++ client/src/Components/Header/Header.js
@@ -3,9 +3,34 @@ import './Header.css';
 function Header() {
   return (
     <div id="header-container">
-        <div className="header-title">watched list</div>
-        <div className="login-container">
-          <button type="button">Login</button>
+        <div id="header-title">
+          <h5>
+            watched list
+          </h5>
+        </div>
+        <div id="header-login-container">
+          <ui>
+            <li>
+              <a href="http://localhost:3080/" className="toggle">
+                My Watched List
+              </a>
+            </li>
+            <li>
+              <a href="http://localhost:3080/" className="toggle">
+                Account
+              </a>
+            </li>
+            <li>
+              <a href="http://localhost:3080/login" className="btn red darken-1 toggle">
+                <i class="fab fa-google left"></i> Log In With Google
+              </a>
+            </li>
+            <li>
+              <a href="http://localhost:3080/" className="toggle">
+                Logout
+              </a>
+            </li>
+          </ui>
         </div>
     </div>
   );
