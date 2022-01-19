9e95587e7065eb80c2f2f8048ea5d75332c7cfec
Hendson <hendsonszeto@live.com>
2022-01-19 13:51:08 -0800

    Change links to correct routes

--- client/src/Components/Header/Header.js
+++ client/src/Components/Header/Header.js
@@ -17,11 +17,10 @@ function Header() {
               <ui>
                 <li><a href="http://localhost:3080/">My Watched List</a></li>
                 <li><a href="http://localhost:3080/">Account</a></li>
-                <li><a href="http://localhost:3000">Logout</a></li>
+                <li><a href="http://localhost:3080/auth/logout">Logout</a></li>
               </ui>
             </div>
-          :
-          <li><a href="http://localhost:3080/login" className="btn red darken-1"><i className="fab fa-google left"></i> Log In With Google</a></li>
+          : <li><a href="http://localhost:3080/auth/google" className="btn red darken-1"><i className="fab fa-google left"></i> Log In With Google</a></li>
         }
     </div>
   );
