c6d25c91d44be95cb0c5314d36bf125daa34f5d3
Hendson <hendsonszeto@live.com>
2022-01-22 10:05:29 -0800

    Create temporary files to pass profile to client

--- client/src/Components/Account/Account.js
+++ client/src/Components/Account/Account.js
@@ -1,6 +1,20 @@
 function Account() {
+
+    const getProfile = async () => {
+        let response = await fetch('http://localhost:3080/');
+
+        if (!response.ok) {
+            throw new Error('Status: $(response.status');
+        }
+
+        return response;
+    }
+
     return(
-        <p>account</p>
+        <div>
+            <p>account</p>
+            <p>Hi {console.log(getProfile())}</p>
+        </div>
     )
 }
 
