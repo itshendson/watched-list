3ef3dccdbfdb3ca95ca5e28f95cc8575cb2f1caf
Hendson <hendsonszeto@live.com>
2022-01-23 14:25:20 -0800

    Delete unused routes in index.js

--- routes/index.js
+++ routes/index.js
@@ -2,14 +2,6 @@ const express = require('express');
 const router = express.Router();
 const User = require('../models/User')
 
-router.get('/login', (req, res) => {
-  res.send('Please login to access your Watched List')
-})
-
-router.get('/mylist', (req, res) => {
-  res.send('Create, edit, remove your Watched List')
-})
-
 router.get("/profile", (req, res) => {
   console.log({ googleId: req.user.id });
   User.findOne({ googleId: req.user.id })
