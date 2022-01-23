53e3dcb312a9e8e9df2d7a706503abfc56158902
Hendson <hendsonszeto@live.com>
2022-01-22 20:37:47 -0800

    Fix bug where findOne used bad argument

--- routes/index.js
+++ routes/index.js
@@ -3,23 +3,23 @@ const router = express.Router();
 const User = require('../models/User')
 
 router.get('/login', (req, res) => {
-    res.send('Please login to access your Watched List')
+  res.send('Please login to access your Watched List')
 })
 
 router.get('/mylist', (req, res) => {
-    res.send('Create, edit, remove your Watched List')
+  res.send('Create, edit, remove your Watched List')
 })
 
 router.get("/profile", function (req, res) {
-    User.findOne({ email: req.session.passport.user.email })
-      .lean()
-      .exec((err, user) => {
-        if (err) console.log(err);
-        if (user) res.status(200).send(user);
-        else {
-          res.status(404).send();
-        }
-      });
-  });
+  User.findOne({ googleId: req.user.id })
+    .lean()
+    .exec((err, user) => {
+      if (err) console.log(err);
+      if (user) res.status(200).send(user);
+      else {
+        res.status(404).send();
+      }
+    });
+});
 
 module.exports = router;
